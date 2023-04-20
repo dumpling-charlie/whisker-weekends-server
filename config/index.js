0e50b4cf3f0da17ad434d46ea5b03eb71c26f2a1
Sophie White <sophie.white@mordrid.net>
2023-04-21 00:09:39 +0200

    cors

--- config/index.js
+++ config/index.js
@@ -9,12 +9,12 @@ const FRONTEND_URL = process.env.ORIGIN || "http://localhost:3000";
 module.exports = (app) => {
   app.set("trust proxy", 1);
 
-  app.use(
-    cors({
-      credentials: true,
-      origin: [FRONTEND_URL],
-    })
-  );
+  // app.use(
+  //   cors({
+  //     credentials: true,
+  //     origin: [FRONTEND_URL],
+  //   })
+  // );
 
   // In development environment the app logs
   app.use(logger("dev"));
