b53269694a20d8353c2a635647d5bec5865d49f9
Sophie White <sophie.white@mordrid.net>
2023-04-20 09:39:34 +0200

    chat

--- server.js
+++ server.js
@@ -1,8 +1,41 @@
 const app = require("./app");
 
+
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 5005
 const PORT = process.env.PORT || 5005;
 
-app.listen(PORT, () => {
+const cors = require('cors');
+const http = require('http').Server(app);
+const socketIO = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+  },
+});
+
+app.use(cors());
+let users = [];
+
+socketIO.on("connection", (socket) => {
+  console.log(`⚡: ${socket.id} user just connected!`);
+  socket.on("message", (data) => {
+    socketIO.emit("messageResponse", data);
+  });
+
+  socket.on("typing", (data) => socket.broadcast.emit("typingResponse", data));
+
+  socket.on("newUser", (data) => {
+    users.push(data);
+    socketIO.emit("newUserResponse", users);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("🔥: A user disconnected");
+    users = users.filter((user) => user.socketID !== socket.id);
+    socketIO.emit("newUserResponse", users);
+    socket.disconnect();
+  });
+});
+
+http.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
 });
